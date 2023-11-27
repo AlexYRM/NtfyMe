7609e204886cbb3c39f081777bc7491b89a8a976
AlexYRM <alexirimia31@gmail.com>
2023-11-27 20:51:43 +0200

    change config file to be able to retrieve secrets from DOPPLER.COM

--- config.py
+++ config.py
@@ -1,19 +1,40 @@
 import os
+import json
 import dotenv
+import requests
 dotenv.load_dotenv()
 
 
 class Config:
     def __init__(self):
-        self.exchange = os.getenv("server_exchange")
-        self.fuelprice = os.getenv("server_fuelprice")
-        self.squash_url = os.getenv("server_squash_url")
-        self.squash_cookies = os.getenv("squash_cookies")
-        self.squash_payload = os.getenv("squash_payload")
+        self.doppler_token = os.getenv("DOPPLER_TOKEN")
+        self.url = "https://api.doppler.com/v3/configs/config/secrets/download?format=json"
+        self.exchange = ""
+        self.fuelprice = ""
+        self.squash_url = ""
+        self.squash_cookies = ""
+        self.squash_payload = ""
+        self.parsing_data()
 
-    def create_ntfy_topic_name(self, station_id: str):
-        topic_name = f"https://ntfy.sh/{self.fuelprice}{station_id.replace('.', '_')}"
-        return topic_name
+    # Function to download secrets from doppler server by sending a get request
+    def download_secrets(self, token):
+        payload = {}
+        headers = {"Authorization": f"Bearer {token}"}
+        response = requests.request("GET", self.url, headers=headers, data=payload)
+        return response.text
+
+    # Function to download and parse data from the Doppler and store them in the class attributes
+    def parsing_data(self):
+        # Download secrets using the provided token
+        json_data = self.download_secrets(self.doppler_token)
+        # Parse the downloaded JSON data
+        data = json.loads(json_data)
+        # Extract specific values from the parsed JSON data and store them in class attributes
+        self.exchange = data.get("SERVER_EXCHANGE")
+        self.fuelprice = data.get("SERVER_FUELPRICE")
+        self.squash_url = data.get("SERVER_SQUASH_URL")
+        self.squash_cookies = data.get("SQUASH_COOKIE")
+        self.squash_payload = data.get("SQUASH_PAYLOAD")
 
 
 config = Config()
